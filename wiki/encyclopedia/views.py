ddaa116695d0acdaeb33b2cc1071aecfcc8f1768
isadorabugarin <57679195+isadorabugarin@users.noreply.github.com>
2021-12-09 12:40:57 -0300

    New Entry Working

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -10,31 +10,30 @@ def entry(request,entry):
     if content is None:
         return render(request, "encyclopedia/notfound.html", {
             "entry": entry,
-            "form": NewTaskForm()
+            "form": SearchForm()
         })
     else:
         return render(request, "encyclopedia/entry.html", {
             "content": markdowner.convert(content),
             "entry": entry,
-            "form": NewTaskForm()
+            "form": SearchForm()
         })
 
 #INDEX PAGE
 def index(request):
     return render(request, "encyclopedia/index.html", {
             "entries": util.list_entries(),
-            "form": NewTaskForm()
+            "form": SearchForm()
         })
 
 #SEARCH PAGE
-class NewTaskForm(forms.Form):
+class SearchForm(forms.Form):
     search = forms.CharField(label="Search")
-
 
 def search(request):
     if request.method == "GET":
         markdowner = Markdown()
-        form = NewTaskForm(request.GET)
+        form = SearchForm(request.GET)
         if form.is_valid():
             value = form.cleaned_data["search"]
             content = util.get_entry(value)
@@ -49,14 +48,15 @@ def search(request):
                         "value": value,
                         "form": form
                         })
+
                 return render(request, "encyclopedia/notfound.html", {
                             "entry":value,
-                             "form": form
+                             "form": SearchForm()
                         })
             else:
                 return render(request, "encyclopedia/search.html", {
                     "entry": markdowner.convert(content),
-                    "form": form
+                    "form": SearchForm()
                     })
         else:
             return render(request, "encyclopedia/index.html", {
@@ -67,20 +67,62 @@ def search(request):
 
 #NEW ENTRY PAGE
 class NewEntryForm(forms.Form):
-    title = forms.CharField(label="Title")
+    entry = forms.CharField(label="Entry", widget=forms.TextInput(attrs={'class': 'form-control col-md-8 col-lg-g'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control col-md-8 col-lg-8','rows':10}))
+    edit = forms.BooleanField(initial=False, widget=forms.HiddenInput(), required=False)
+
 
 def new(request):
     if request.method == "POST":
         form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["entry"]
+            content = form.cleaned_data["content"]
+            if(util.get_entry(title) is None or form.cleaned_data["edit"] is True ):
+                util.save_entry(title,content)
+                return render(request, "encyclopedia/entry.html", {
+                    "content": content,
+                    "form":SearchForm()
+                })
+            else:
+                return render(request, "encyclopedia/new.html", {
+                        "form2": form,
+                        "existing": True,
+                        "entry": title,
+                        "form": SearchForm()
+                })
+        else:
+            return render(request, "encyclopedia/new.html", {
+            "form2": form,
+            "existing": False,
+            "form": SearchForm()
+            })
+    else:
         return render(request, "encyclopedia/new.html", {
-            "form": form
+        "form2": NewEntryForm(),
+        "form": SearchForm()
         })
-    return render(request, "encyclopedia/new.html",{
-        "form": NewEntryForm()
-    })
 
 #EDIT ENTRY PAGE
 
+def edit(request, entry):
+    content = util.get_entry(entry)
+    if content is None:
+        return render(request, "encyclopedia.notfound.html", {
+            "entry":entry
+        })
+    else:
+        form = NewEntryForm()
+        form.fields["entry"].initial = entry
+        form.fields["entry"].widget = forms.HiddenInput()
+        form.fields["entry"].initial = content
+        form.fields["entry"].initial = True
+        return render(request, "encyclopedia/new.html", {
+            "form2":form,
+            "edit":form.fields["edit"].initial,
+            "entry": form.fields["entry"].initial
+
+        })
 #RANDOM PAGE
 
 
